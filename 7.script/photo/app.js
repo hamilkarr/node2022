82526294deceac8ab8fd6c7bb2096c478b133b74
hamilkarr <12uclock@gmail.com>
2022-08-18 19:50:03 +0900

    이미지 자동분류 fs 완료

--- 7.script/photo/app.js
+++ 7.script/photo/app.js
@@ -12,12 +12,12 @@ const folder = process.argv[1];
 // const workingDir = path.join(os.homedir(),'Pictures',folder);
 // console.log(workingDir);
 const upperPath = path.resolve(folder,'../' );
-console.log(folder);
+// console.log(folder);
 console.log(upperPath);
 
 
 
-if(!fs.existsSync(upperPath)) {
+if(!folder || !fs.existsSync(upperPath)) {
     console.error('Please Enter Folder name in Pictures');
     return;
 }
@@ -26,6 +26,58 @@ const videoDir = path.join(upperPath,'video');
 const capturedDir = path.join(upperPath,'captured');
 const duplicatedDir = path.join(upperPath,'duplicated');
 
-console.log(videoDir);
-console.log(capturedDir);
-console.log(duplicatedDir);
+// console.log(videoDir);
+// console.log(capturedDir);
+// console.log(duplicatedDir);
+
+!fs.existsSync(videoDir) && fs.mkdirSync(videoDir);  // && 조건문 다시 복습 필요
+!fs.existsSync(capturedDir) && fs.mkdirSync(capturedDir);  // && 조건문 다시 복습 필요
+!fs.existsSync(duplicatedDir) && fs.mkdirSync(duplicatedDir);  // && 조건문 다시 복습 필요
+
+// const array = fs.readdir(upperPath,{encoding: "utf-8",withFileTypes:false}, (error) => console.error(error));
+// console.log(array);
+// return;
+
+fs.promises.readdir(upperPath).then(processFiles).catch(console.error);
+
+function processFiles(files) {
+    files.forEach((file) => {
+        if(isVideoFile(file)) {
+            move(file, videoDir);
+        } else if(isCapturedFile(file)) {
+            move(file, capturedDir);
+        } else if(isDuplicatedFile(files, file)) {
+            move(file, duplicatedDir);
+        }
+    });
+};
+
+function isVideoFile(file) {
+    const regExp = /(mp4|mov)$/gm;
+    const match = file.match(regExp); // return array // == regExp.test(file) return boolean    
+    // const test = regExp.test(file);
+    // console.log(match);
+    // console.log(test);
+    return !!match;
+}
+function isCapturedFile(file) {
+    const regExp = /(png|aae)$/gm;
+    const match  = regExp.test(file);
+    return match;
+}
+function isDuplicatedFile(files, file) {
+    if(!file.startsWith('IMG_') || file.startsWith('IMG_E')) {
+        return false;
+    }
+    const edited = `IMG_E${file.split('_')[1]}`; // IMG_E1234, IMG_1234, IMG_2345
+    const found = files.find(f => f.includes(edited));
+    return !!found;
+}
+
+function move(file, targetDir) {
+    const oldPath = path.join(upperPath, file);
+    const newPath = path.join(targetDir, file);
+    fs.promises
+        .rename(oldPath, newPath)
+        .catch(console.error);        
+}
